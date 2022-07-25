416e3d03ea5e41ed12716e7a0bb6661515cee96a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-26 00:15:25 +0100

    Create Context Provider for Counter State.

---
+++ src/components/CounterContext.jsx
@@ -0,0 +1,13 @@
+import React, {createContext, useState} from 'react';
+
+// creating context
+export const CounterContext = createContext();
+
+// context provider
+export const CounterProvider = (props) => {
+    const [count, setCount] = useState(0);
+
+    return(<CounterContext.Provider value={[count, setCount]}>
+        {props.children}
+        </CounterContext.Provider>);
+}
