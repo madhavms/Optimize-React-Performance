d90c6201338a84db04e7f22ed9185b545c9a687b
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-26 00:12:50 +0100

    Modified the App.js file
    A new component SomeScreen is passed as render props to the Counter component to show that the child component wont re-render in case of state change for the parent.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import Welcome from "./components/Welcome";
+import Counter from "./components/Counter";
 import { CounterProvider } from "./components/CounterContext";
+import SomeScreen from "./components/SomeScreen";
 
 function App() {
+
   return (
     <CounterProvider>
       <div className="App">
-        <Welcome/>
+      <Counter somescreen={<SomeScreen label='Some screen'/>}/> {/* Counter component uses the provider state*/}
       </div>
     </CounterProvider>
   );
 }
+
 export default App;
