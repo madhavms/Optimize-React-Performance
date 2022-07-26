6416cb8f666f7b25899dc5c689c66b09c91dd775
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-07-26 10:58:30 +0100

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,8 @@
 import React, {useContext, useState} from 'react';
 import {CounterContext} from './CounterContext';
-import SomeScreen from './SomeScreen';
 
 const Counter = (props) => {
 const [count, setCount] = useContext(CounterContext);
-const [toggleProps, setToggleProps] = useState(true);
 
 const incrementCount = (e) => {
     setCount(prevCount => prevCount + 1);
@@ -19,9 +17,7 @@ return(
     <p>The count is: {count}</p> {/*state from context provider*/}
     <button onClick={incrementCount}>Increment Count</button>
     <button onClick={resetCount} style={{'margin':'10px'}}>Reset Count</button>
-    <p>{toggleProps ? '(somescreen as render props)' : '(somescreen as child component)'}</p>
-    <button onClick={() => setToggleProps(prev => !prev)}>Toggle Props</button>
-    {toggleProps ? props.somescreen : <SomeScreen/>}
+    
     </div>
 );
 };
